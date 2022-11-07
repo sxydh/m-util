e4212892ddbc28a6d09ddce7a095ae648f31036b
sxydh <sxydhgg@gmail.com>
2022-11-07 17:48:06 +0800

    [ADD][BaiduFaceUtils]addGroup, listGroup, deleteGroup, listUser, getUser, deleteUser, addFace, listFace, deleteFace, searchFace.

--- src/main/java/cn/net/bhe/util/BaiduFaceUtils.java
+++ src/main/java/cn/net/bhe/util/BaiduFaceUtils.java
@@ -43,6 +43,46 @@ public class BaiduFaceUtils {
      * 人脸检测
      */
     private static final String API_DETECT = "/rest/2.0/face/v3/detect";
+    /**
+     * 创建用户组
+     */
+    private static final String API_GROUP_ADD = "/rest/2.0/face/v3/faceset/group/add";
+    /**
+     * 组列表查询
+     */
+    private static final String API_GROUP_LIST = "/rest/2.0/face/v3/faceset/group/getlist";
+    /**
+     * 删除用户组
+     */
+    private static final String API_GROUP_DELETE = "/rest/2.0/face/v3/faceset/group/delete";
+    /**
+     * 获取用户列表
+     */
+    private static final String API_USER_LIST = "/rest/2.0/face/v3/faceset/group/getusers";
+    /**
+     * 用户信息查询
+     */
+    private static final String API_USER_GET = "/rest/2.0/face/v3/faceset/user/get";
+    /**
+     * 删除用户
+     */
+    private static final String API_USER_DELETE = "/rest/2.0/face/v3/faceset/user/delete";
+    /**
+     * 人脸注册
+     */
+    private static final String API_FACE_ADD = "/rest/2.0/face/v3/faceset/user/add";
+    /**
+     * 人脸列表
+     */
+    private static final String API_FACE_LIST = "/rest/2.0/face/v3/faceset/face/getlist";
+    /**
+     * 人脸删除
+     */
+    private static final String API_FACE_DELETE = "/rest/2.0/face/v3/faceset/face/delete";
+    /**
+     * 人脸搜索
+     */
+    private static final String API_FACE_SEARCH = "/rest/2.0/face/v3/search";
 
     public BaiduFaceUtils(
             String clientId,
@@ -112,6 +152,199 @@ public class BaiduFaceUtils {
                         "face_field", "age,beauty,expression,face_shape,gender,glasses,landmark,landmark150,quality,eye_status,emotion,face_type,mask,spoofing",
                         "face_type", "LIVE",
                         "liveness_control", "NORMAL"));
+    }
+
+    /**
+     * 创建用户组
+     *
+     * @param groupId 用户组id
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-sVgQXDpqrMd">创建用户组</a>
+     */
+    public String addGroup(String groupId) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_GROUP_ADD,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "group_id", groupId));
+    }
+
+    /**
+     * 组列表查询
+     *
+     * @param start  起始序号
+     * @param length 返回数量
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-PoFdvitBK3b">组列表查询</a>
+     */
+    public String listGroup(String start, String length) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_GROUP_LIST,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "start", start,
+                        "length", length));
+    }
+
+    /**
+     * 删除用户组
+     *
+     * @param groupId 用户组id
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-Nia7CbnVkwZ">删除用户组</a>
+     */
+    public String deleteGroup(String groupId) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_GROUP_DELETE,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "group_id", groupId));
+    }
+
+    /**
+     * 获取用户列表
+     *
+     * @param groupId 用户组id
+     * @param start   起始序号
+     * @param length  返回数量
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-WwqU2DmXUYN">获取用户列表</a>
+     */
+    public String listUser(String groupId, String start, String length) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_USER_LIST,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "group_id", groupId,
+                        "start", start,
+                        "length", length));
+    }
+
+    /**
+     * 用户信息查询
+     *
+     * @param groupId 用户组id
+     * @param userId  用户id
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-T3aj3knpq9W">用户信息查询</a>
+     */
+    public String getUser(String groupId, String userId) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_USER_GET,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "group_id", groupId,
+                        "user_id", userId));
+    }
+
+    /**
+     * 删除用户
+     *
+     * @param groupId 用户组id
+     * @param userId  用户id
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-sjbaBxxM46u">删除用户</a>
+     */
+    public String deleteUser(String groupId, String userId) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_USER_DELETE,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "group_id", groupId,
+                        "user_id", userId));
+    }
+
+    /**
+     * 人脸注册
+     *
+     * @param image    图片信息
+     * @param groupId  用户组id
+     * @param userId   用户id
+     * @param userInfo 用户资料
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-eSX4WxmdzLE">人脸注册</a>
+     */
+    public String addFace(String image, String groupId, String userId, String userInfo) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_FACE_ADD,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "image", image,
+                        "image_type", "BASE64",
+                        "group_id", groupId,
+                        "user_id", userId,
+                        "user_info", userInfo,
+                        "quality_control", "NORMAL",
+                        "liveness_control", "NORMAL",
+                        "action_type", "APPEND"));
+    }
+
+    /**
+     * 获取用户人脸列表
+     *
+     * @param groupId 用户组id
+     * @param userId  用户id
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-8D5DsUtNHhT">获取用户人脸列表</a>
+     */
+    public String listFace(String groupId, String userId) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_FACE_LIST,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "group_id", groupId,
+                        "user_id", userId));
+    }
+
+    /**
+     * 人脸删除
+     *
+     * @param logId     请求标识码
+     * @param groupId   用户组id
+     * @param userId    用户id
+     * @param faceToken 需要删除的人脸图片token
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-GKx4KDjYuvZ">人脸删除</a>
+     */
+    public String deleteFace(String logId, String groupId, String userId, String faceToken) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_FACE_DELETE,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "logId", logId,
+                        "group_id", groupId,
+                        "user_id", userId,
+                        "face_token", faceToken));
+    }
+
+    /**
+     * 人脸搜索
+     *
+     * @param image       请求标识码
+     * @param groupIdList 从指定的group中进行查找
+     * @param maxUserNum  查找后返回的用户数量。返回相似度最高的几个用户，默认为1，最多返回50个。
+     * @return result
+     * @throws Exception exception
+     * @see <a href="https://cloud.baidu.com/apiexplorer/index.html?Product=GWSE-nmhroEsyriA&Api=GWAI-adiJw5iQYte">人脸搜索</a>
+     */
+    public String searchFace(String image, String groupIdList, String maxUserNum) throws Exception {
+        return post(() -> HttpClientUtils.uri(SERVER + API_FACE_SEARCH,
+                        "access_token", accessToken),
+                () -> HttpClientUtils.buildHeaders(HDN_CT, HDV_AJ),
+                () -> Map.of(
+                        "image", image,
+                        "image_type", "BASE64",
+                        "group_id_list", groupIdList,
+                        "max_user_num", maxUserNum));
     }
 
 }
--- src/test/java/cn/net/bhe/util/BaiduFaceUtilsTest.java
+++ src/test/java/cn/net/bhe/util/BaiduFaceUtilsTest.java
@@ -1,13 +1,17 @@
 package cn.net.bhe.util;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
 
 class BaiduFaceUtilsTest {
 
     static final BaiduFaceUtils baiduFaceUtils = new BaiduFaceUtils("", "");
+
 
     @Test
     void getToken() {
@@ -29,4 +33,126 @@ class BaiduFaceUtilsTest {
         }
     }
 
+    @Test
+    void addGroup() {
+        try {
+            String ret = baiduFaceUtils.addGroup("group02");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void listGroup() {
+        try {
+            String ret = baiduFaceUtils.listGroup("0", "10");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void deleteGroup() {
+        try {
+            String ret = baiduFaceUtils.deleteGroup("group_test1");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void listUser() {
+        try {
+            String ret = baiduFaceUtils.listUser(
+                    "group01",
+                    "0",
+                    "10");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void getUser() {
+        try {
+            String ret = baiduFaceUtils.getUser(
+                    "group01",
+                    "user01");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void deleteUser() {
+        try {
+            String ret = baiduFaceUtils.deleteUser(
+                    "group01",
+                    "user01");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void addFace() {
+        try {
+            String ret = baiduFaceUtils.addFace(
+                    IOUtils.resourceToString("/BaiduFaceUtilsTest_face01.txt", StandardCharsets.UTF_8),
+                    "group01",
+                    "user01",
+                    JSON.toJSONString(Map.of(
+                            "age", "18",
+                            "gender", "male",
+                            "job", "boss")));
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void listFace() {
+        try {
+            String ret = baiduFaceUtils.listFace(
+                    "group01",
+                    "user01");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void deleteFace() {
+        try {
+            String ret = baiduFaceUtils.deleteFace(
+                    UUID.randomUUID().toString(),
+                    "group01",
+                    "user01",
+                    "291524638c0280320f558b83bf885336");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void searchFace() {
+        try {
+            String ret = baiduFaceUtils.searchFace(
+                    IOUtils.resourceToString("/BaiduFaceUtilsTest_face01.txt", StandardCharsets.UTF_8),
+                    "group01",
+                    "1");
+            System.out.println(ret);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
