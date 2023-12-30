bcd2bb27b248e5add88ed9a6f00dd99d07ad3743
sxydh <sxydhgg@gmail.com>
2023-12-30 15:35:51 +0800

    [UPDATE]

---
+++ src/main/java/cn/net/bhe/mutil/WdUtils.java
@@ -0,0 +1,34 @@
+package cn.net.bhe.mutil;
+
+import java.util.Random;
+
+public class WdUtils {
+
+    private static final StringBuilder[] WORD_ARR;
+    private static final Random RANDOM = new Random();
+
+    static {
+        try {
+            String[] wordArr = ZipUtils.deComp("WdUtils.WORD_ARR").split("#");
+            WORD_ARR = new StringBuilder[wordArr.length];
+            for (int i = 0; i < wordArr.length; i++) {
+                WORD_ARR[i] = new StringBuilder().append(wordArr[i]);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String random() {
+        return random(true);
+    }
+
+    public static String random(boolean pascal) {
+        StringBuilder word = WORD_ARR[RANDOM.nextInt(WORD_ARR.length)];
+        if (pascal) {
+            word.setCharAt(0, (char) (word.charAt(0) - 32));
+        }
+        return word.toString();
+    }
+
+}
