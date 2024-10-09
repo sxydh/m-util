2ec542fd6bd69a5cdd3dd868fd70d12a29c46c00
sxydh <sxydhgg@gmail.com>
2024-10-09 12:25:12 +0800

    [UPDATE]

--- src/main/java/cn/net/bhe/mutil/As.java
+++ src/main/java/cn/net/bhe/mutil/As.java
@@ -1,5 +1,6 @@
 package cn.net.bhe.mutil;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
 
 public class As {
@@ -8,13 +9,30 @@ public class As {
         isTrue(bool, StrUtils.EMPTY);
     }
 
+    public static void isTrue(boolean bool, Class<? extends RuntimeException> exClass) {
+        isTrue(bool, StrUtils.EMPTY, exClass);
+    }
+
+    public static void isTrue(boolean bool, String msg) {
+        isTrue(bool, msg, IllegalArgumentException.class);
+    }
+
     public static void isTrue(Supplier<Boolean> supplier, String msg) {
         isTrue(supplier.get(), msg);
     }
 
-    public static void isTrue(boolean bool, String msg) {
+    public static void isTrue(boolean bool, String msg, Class<? extends RuntimeException> exClass) {
         if (!bool) {
-            throw new IllegalArgumentException(msg);
+            RuntimeException exInstance;
+            try {
+                exInstance = exClass.getConstructor(String.class).newInstance(msg);
+            } catch (NoSuchMethodException |
+                     InstantiationException |
+                     IllegalAccessException |
+                     InvocationTargetException e) {
+                throw new RuntimeException(e);
+            }
+            throw exInstance;
         }
     }
 
