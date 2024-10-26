b38c34605314628284625adf90d5108911105ba5
sxydh <sxydhgg@gmail.com>
2024-10-26 16:07:26 +0800

    [UPDATE]

--- src/main/java/cn/net/bhe/mutil/FileServerUtils.java
+++ src/main/java/cn/net/bhe/mutil/FileServerUtils.java
@@ -181,7 +181,7 @@ public class FileServerUtils {
 
             try (InputStream is = new FileInputStream(file);
                  OutputStream os = httpExchange.getResponseBody()) {
-                byte[] buffer = new byte[1024 * 1024];
+                byte[] buffer = new byte[8192];
                 int read;
                 while ((read = is.read(buffer)) != -1) {
                     os.write(buffer, 0, read);
