bf6de00a77a119fd664f4b996261fd2c2e76e89c
sxydh <sxydhgg@gmail.com>
2024-10-26 15:38:09 +0800

    [UPDATE]

--- src/main/java/cn/net/bhe/mutil/UrlUtils.java
+++ src/main/java/cn/net/bhe/mutil/UrlUtils.java
@@ -1,5 +1,6 @@
 package cn.net.bhe.mutil;
 
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -7,6 +8,10 @@ public class UrlUtils {
 
     public static String encode(String url) {
         return URLEncoder.encode(url, StandardCharsets.UTF_8).replace(StrUtils.PLUS, StrUtils.SPACE_ENCODE);
+    }
+
+    public static String decode(String url) {
+        return URLDecoder.decode(url, StandardCharsets.UTF_8);
     }
 
 }
